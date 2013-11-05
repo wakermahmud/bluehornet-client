54d467b12d15879d9ec53f71969fd55b84f1e93a
Andrew Turley <aturley@theladders.com>
2013-11-05 11:43:13 -0500

    Bump version from 0.1.0 to 0.1.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject theladders/bluehornet-client "0.1.0"
+(defproject theladders/bluehornet-client "0.1.1-SNAPSHOT"
   :description "A library for interacting with BlueHornet's API using Clojure."
   :aot :all
   :dependencies [[org.clojure/clojure "1.4.0"]
