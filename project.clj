20f68732f05286b32d32f8b17e5e9c74ff6349ae
Derek Brown <derekbrown121@gmail.com>
2013-11-07 11:52:27 -0500

    Version to 0.1.2-SNAPSHOT; added :plugins and :repositories.

--- project.clj
+++ project.clj
@@ -1,6 +1,16 @@
-(defproject theladders/bluehornet-client "0.1.1-SNAPSHOT"
+(defproject theladders/bluehornet-client "0.1.2-SNAPSHOT"
   :description "A library for interacting with BlueHornet's API using Clojure."
   :aot :all
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.xml "0.0.7"]]
+  :plugins 
+  [   
+     [theladders/lein-uberjar-deploy "0.1.2"]
+  ]
+
+  :repositories
+  [
+     ["snapshots" {:id "nexus" :url "http://host/nexus/content/repositories/snapshots"}]
+     ["releases"  {:id "nexus" :url "http://host/nexus/content/repositories/releases"}]
+  ]
   )
