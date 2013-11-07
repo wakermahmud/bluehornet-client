17cba254bb17fc2ecf9fde351c0b9a6dbd4c84b1
Andrew Turley <aturley@theladders.com>
2013-11-07 16:16:22 -0500

    Add transactional.updateTemplate.

--- src/theladders/bluehornet_client/api/transactional.clj
+++ src/theladders/bluehornet_client/api/transactional.clj
@@ -3,3 +3,6 @@
 
 (defn send-transaction [email template-id & {:as optional}]
   (core/make-method "transactional.sendTransaction" :email email :template_id template-id :optional optional))
+
+(defn update-template [template-id & {:as optional}]
+  (core/make-method "transactional.updateTemplate" :template_id template-id :optional optional))
