f917316aa687f019e1018ba689c74240b5dec54b
Andrew Turley <aturley@theladders.com>
2013-11-08 10:25:59 -0500

    Add support for transactional.rebuildTemplate.

--- src/theladders/bluehornet_client/api/transactional.clj
+++ src/theladders/bluehornet_client/api/transactional.clj
@@ -6,3 +6,6 @@
 
 (defn update-template [template-id & {:as optional}]
   (core/make-method "transactional.updateTemplate" :template_id template-id :optional optional))
+
+(defn rebuild-template [template-id]
+  (core/make-method "transactional.rebuildTemplate" :template_id template-id))
