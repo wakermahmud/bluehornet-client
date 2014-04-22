b94f908d0af4359e41c5957a00bc8c54ad767f8b
Andrew Turley <aturley@theladders.com>
2014-04-22 16:47:56 -0400

    Add a repl profile that includes clj-http for testing.

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :aot :all
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.xml "0.0.7"]]
+
+  :profiles {:repl {:dependencies [[clj-http "0.9.1"]]}}
+
   :plugins 
   [   
      [theladders/lein-uberjar-deploy "0.1.2"]
