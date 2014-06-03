4fa99008c3de66445889090d7559ad5d255b38c3
John Connolly <jeconnol@gmail.com>
2014-06-03 11:23:41 -0400

    add function for legacy.delete_subscribers

--- src/theladders/bluehornet_client/api/legacy.clj
+++ src/theladders/bluehornet_client/api/legacy.clj
@@ -13,6 +13,15 @@
                     :filename filename
                     :optional optional))
 
+(defn delete-subscribers [reply-email ftp-server ftp-username ftp-password filename & {:as optional}]
+  (core/make-method "legacy.delete_subscribers"
+                    :reply_email reply-email
+                    :ftp_server ftp-server
+                    :ftp_user_name ftp-username
+                    :ftp_user_pass ftp-password
+                    :filename filename
+                    :optional optional))
+
 (defn retrieve-active [& {:as optional}]
   (core/make-method "legacy.retrieve_active" :optional optional))
 
